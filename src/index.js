2151662f75e75fcb9f6ed48f8ae035e0d4359e27
shartzzz <shartzzz@macBook.local>
2025-02-24 20:54:15 +0300

    проверка на работоспособность через index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import { default as Bot } from "./core/Bot";
+
+const bot = new Bot(process.env.BOT_TOKEN);
+
+bot.start();
+
+bot.sendMessage(process.env.CHAT_ID, "test");
+
+bot.getUpdates(1);
+
+bot.stop();
